a7c2582b0b0aff68c916826f4e8ee111a8936c16
Scott Moses Sunarto <smsunarto007@gmail.com>
2020-12-19 03:18:40 +0700

    add email subscribe integration

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// Google Form Redirect
+// eslint-disable-next-line
+var submitted = false;
+
+// Google Form Submit Text Confirmation
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const subscribed = urlParams.get("subscribed");
+
+if (subscribed == "true") {
+    var element = document.getElementById("email-subscribed");
+    element.classList.add("d-block");
+}
+
+// Navbar Background
 const navbar = document.querySelector(".navbar");
 window.onscroll = () => {
     if (window.scrollY > 20) {
