4ca9c9337d2c973f014b5f8d8e2e60bcf734dcd7
Scott Moses Sunarto <smsunarto007@gmail.com>
2021-01-09 23:45:37 +0700

    fix opengraph bug

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// Google Form Redirect
-// eslint-disable-next-line
-var submitted = false;
-
-// Google Form Submit Text Confirmation
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const subscribed = urlParams.get("subscribed");
-
-if (subscribed == "true") {
-    var element = document.getElementById("email-subscribed");
-    element.classList.add("d-block");
-}
-
 // Navbar Background
 const navbar = document.querySelector(".navbar");
 window.onscroll = () => {
